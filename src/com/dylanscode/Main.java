b3a33f003e58ae8ef71668b827ce17d5c1f6b4cb
dylansmall6 <dsmall105@gmail.com>
2017-10-12 21:26:43 -0700

    removed jframe

--- src/com/dylanscode/Main.java
+++ src/com/dylanscode/Main.java
@@ -1,24 +1,21 @@
 package com.dylanscode;
 
-import java.awt.Dimension;
-import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
-import javax.swing.JFrame;
 
 import com.dylanscode.terrain.TerrainGenerator;
 
 public class Main
 {
-	TerrainGenerator generator;
-
+	private TerrainGenerator generator;
+	private BufferedImage terrain;
 	public void start()
 	{
 		generator = new TerrainGenerator(1920, 1080);
-		BufferedImage terrain = null;
+		terrain = null;
 		try
 		{
 			terrain = generator.generateImage();
@@ -27,18 +24,6 @@ public class Main
 		} catch (IOException e)
 		{
 			e.printStackTrace();
-		}finally {
-			JFrame frame = new JFrame("Terrain");
-			frame.setMinimumSize(new Dimension(generator.width, generator.height));
-			frame.setMaximumSize(new Dimension(generator.width, generator.height));
-			frame.setPreferredSize(new Dimension(generator.width, generator.height));
-			frame.setSize(generator.width, generator.height);
-			frame.setResizable(false);
-			frame.setVisible(true);
-			frame.pack();
-			Graphics2D g = (Graphics2D) frame.getGraphics();
-			g.drawImage(terrain, 0, 0, null);
-			
 		}
 	}
 
