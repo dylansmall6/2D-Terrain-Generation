64aef947e46c57b868b6c4d400a72f8db98a71c9
dylansmall6 <dsmall105@gmail.com>
2017-10-12 21:11:31 -0700

    Fixed some color errors, making generation more realistic

--- src/com/dylanscode/Main.java
+++ src/com/dylanscode/Main.java
@@ -1,9 +1,13 @@
 package com.dylanscode;
 
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.swing.JFrame;
 
 import com.dylanscode.terrain.TerrainGenerator;
 
@@ -14,13 +18,27 @@ public class Main
 	public void start()
 	{
 		generator = new TerrainGenerator(1920, 1080);
+		BufferedImage terrain = null;
 		try
 		{
+			terrain = generator.generateImage();
 			//saves the image to a png file
-			ImageIO.write(generator.generateImage(), "png", new File("Terrain.png"));
+			ImageIO.write(terrain, "png", new File("Terrain.png"));
 		} catch (IOException e)
 		{
 			e.printStackTrace();
+		}finally {
+			JFrame frame = new JFrame("Terrain");
+			frame.setMinimumSize(new Dimension(generator.width, generator.height));
+			frame.setMaximumSize(new Dimension(generator.width, generator.height));
+			frame.setPreferredSize(new Dimension(generator.width, generator.height));
+			frame.setSize(generator.width, generator.height);
+			frame.setResizable(false);
+			frame.setVisible(true);
+			frame.pack();
+			Graphics2D g = (Graphics2D) frame.getGraphics();
+			g.drawImage(terrain, 0, 0, null);
+			
 		}
 	}
 
--- src/com/dylanscode/terrain/TerrainGenerator.java
+++ src/com/dylanscode/terrain/TerrainGenerator.java
@@ -11,8 +11,8 @@ import com.dylanscode.perlin.NoiseGenerator;
 
 public class TerrainGenerator
 {
-	private int width;
-	private int height;
+	public int width;
+	public int height;
 	private Board board;
 
 	public TerrainGenerator(int width, int height)
@@ -43,38 +43,38 @@ public class TerrainGenerator
 			color = generateColor(noise);
 		}
 		/**
-		 * noise is between 0 and 1, so we must allow colors for those colors
+		 * noise is between 0 and 1, so we will assign colors to different noise values
 		 * @param noise
-		 * @return
+		 * @return color of noise value
 		 */
 		private Color generateColor(float noise) {
-			if(noise <= .25) {
+			if(noise <= .55) {
 				//deep blue
 				return new Color(1,40,104);
-			}else if(noise > .25 && noise <= .42) {
+			}else if(noise > .55 && noise <= .62) {
 				//light blue
 				return new Color(3,65,165);
-			}else if(noise > .42 && noise <= .45) {
+			}else if(noise > .62 && noise <= .65) {
 				//sandy color
 				return new Color(242, 240, 152);
-			}else if(noise >.45 && noise <=.55) {
+			}else if(noise >.65 && noise <=.7) {
 				//light green
 				return new Color(48, 168, 42);
-			}else if(noise > .55 && noise <= .6) {
+			}else if(noise > .7 && noise <= .8) {
 				//middle green
 				return new Color(4, 135, 0);
-			}else if(noise > .6 && noise <= .7) {
+			}else if(noise > .8 && noise <= .85) {
 				//dark green
 				return new Color(9,89,5);
 			}
-			else if(noise > .7 && noise <= .78) {
+			else if(noise > .85 && noise <= .89) {
 				//dark gray
 				return new Color(178, 178, 178);
-			}else if(noise > .78 && noise <= .85) {
+			}else if(noise > .89 && noise <= .92) {
 				//light gray
 				return new Color(91, 91, 89);
 			}
-			else if(noise > .85 && noise <= .95) {
+			else if(noise > .92 && noise <= .95) {
 				//very light gray
 				return new Color(224, 219, 219);
 			}else {
@@ -92,7 +92,7 @@ public class TerrainGenerator
 
 	private class Board
 	{
-		protected int scale = 1, rows, cols, increment = 2;
+		protected int scale = 1, rows, cols, increment = 1;
 		private NoiseGenerator generator;
 		private Cell[][] cells;
 		protected Board(int width, int height)
